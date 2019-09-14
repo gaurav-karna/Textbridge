f3d66975eee599ed7d3c1d1a7f17825e5356051d
gaurav-karna <gaurav.karna@mail.mcgill.ca>
2019-09-14 02:31:57 -0400

    refactored

--- TextbridgeApp/urls.py
+++ TextbridgeApp/urls.py
@@ -3,11 +3,10 @@ from django.contrib import admin
 from . import views
 import django.contrib.auth
 from django.conf import settings
-# import django.contrib.auth.views as auth_views
 
 urlpatterns = [
     # Here is where all of the routes will be
-    # in regards to the Buddy web application
+    # in regards to the web application
 
     # public paths, open to everyone // self-explanatory
     path('', views.index, name='index'),
