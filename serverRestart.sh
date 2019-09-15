71071363f7688bf1e3e5a0356ceea9a694daf877
gaurav-karna <gaurav.karna@mail.mcgill.ca>
2019-09-15 00:35:37 -0400

    tracking front end

--- serverRestart.sh
+++ serverRestart.sh
@@ -1,1 +1,64 @@
-#!/usr/bin/env bash
+# script to restart servers upon redeployment automatically
+echo "###---Script to restart servers---###"
+
+echo
+
+# sleep cycles used to allow breakage between commands
+# no need to force restarts.
+echo "###---Phase 1 of 4---###"
+echo -n "Restarting gunicorn daemon"
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo "."
+# VPS COMMAND - won't work locally.
+sudo systemctl daemon-reload
+echo "###---Phase 1 of 4 DONE---###"
+
+echo ""
+
+echo "###---Phase 2 of 4---###"
+echo -n "Restarting gunicorn server"
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo "."
+# VPS COMMAND - won't work locally.
+sudo systemctl restart gunicorn
+echo "###---Phase 2 of 4 DONE---###"
+
+echo ""
+
+echo "###---Phase 3 of 4---###"
+echo -n "Performing nginx test"
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo "."
+# VPS COMMAND - won't work locally.
+sudo nginx -t
+echo "###---Phase 3 of 4 DONE---###"
+
+echo ""
+
+echo "###---Phase 4 of 4---###"
+echo -n "Restarting nginx reverse proxy"
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo "."
+# VPS COMMAND - won't work locally.
+sudo systemctl restart nginx
+echo "###---Phase 4 of 4 DONE---###"
+
+echo ""
+
+echo "Successful reboot. Check to make sure with sudo systemctl status [gunicorn/nginx]"
