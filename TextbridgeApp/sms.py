01390191f65988627d26a02da375b87fdf638aa8
gaurav-karna <gaurav.karna@mail.mcgill.ca>
2019-09-14 20:01:19 -0400

    removed incomplete function

--- TextbridgeApp/sms.py
+++ TextbridgeApp/sms.py
@@ -2,15 +2,14 @@ from twilio.rest import Client
 
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
-import secrets
+
+from Textbridge.secrets import *
 app = Flask(__name__)
-# Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
 
 
 class SendRecTexts:
-    account_sid = secrets.twilio_account_sid
-    auth_token = secrets.twilio_auth_token
+    account_sid = twilio_account_sid
+    auth_token = twilio_auth_token
     def sendTexts(self,content,to_):
         client = Client(self.account_sid, self.auth_token)
 
@@ -22,6 +21,6 @@ class SendRecTexts:
                         )
 
         print(message.sid)
-    def RecText(self)
+
 obj = SendRecTexts()
 # obj.sendTexts("Send this to me","+16476716466")
