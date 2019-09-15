cdf2eb94fbd4d01a3d9c69c0c16f07ac7f368bf7
gaurav-karna <gaurav.karna@mail.mcgill.ca>
2019-09-15 05:44:38 -0400

    testing use rlist

--- TextbridgeApp/views.py
+++ TextbridgeApp/views.py
@@ -84,8 +84,10 @@ def get_user_friends(from_number):
 
 def get_to_number(to_name: str):
     user_list = User.objects.all()
-    for entry in user_list:
-        return entry.first_name + entry.last_name
+    if not user_list:
+        return "this is what it is"
+    # for entry in user_list:
+    #     return entry.first_name + entry.last_name
         # if "{} {}".format(entry.first_name, entry.last_name).lower() == to_name.lower():
         #     return "this works"
     #         return entry.social_auth.extra_data['phone_number']
