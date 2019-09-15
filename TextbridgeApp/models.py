c53fbf9ca0f182ba52a6990b2a9e3af469c8cf2b
gaurav-karna <gaurav.karna@mail.mcgill.ca>
2019-09-15 06:28:56 -0400

    hail mary: 2

--- TextbridgeApp/models.py
+++ TextbridgeApp/models.py
@@ -7,4 +7,4 @@ class TextbridgeUser(models.Model):
     Phone_Number = models.CharField(max_length=12, verbose_name='Phone Number', default=None,
                                     help_text='Format your number like: +12345678910')
 
-    Backup_Name = models.CharField(max_length=64, verbose_name='Backup Referring Name', default=None)
+    Backup_Name = models.CharField(max_length=64, verbose_name='Backup Referring Name', default=None, null=True)
